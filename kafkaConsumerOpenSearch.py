ba223001d4f6d2e3f0731869c4f3b975172c54aa
samar.mami18@gmail.com <samar.mami18@gmail.com>
2023-08-14 21:08:03 +0200

    first commit after tests

---
+++ kafkaConsumerOpenSearch.py
@@ -0,0 +1,108 @@
+import json
+from urllib.parse import urlparse
+from opensearchpy import OpenSearch
+from confluent_kafka import Consumer
+import config
+
+
+# Create an OpenSearch client and an index
+def create_opensearch_client():
+    conn_string = "http://localhost:9200"
+
+    # Parse the connection string
+    conn_uri = urlparse(conn_string)
+    # Create a connection to the OpenSearch REST client
+    # Create an index
+    try:
+        client = OpenSearch([{'host': conn_uri.hostname, 'port': conn_uri.port}],
+                            use_ssl=False, verify_certs=False)
+        index_name = 'wikimedia'
+        if not client.indices.exists(index_name):
+            client.indices.create(index_name)
+            print('The wikimedia index has been created')
+        else:
+            print('Index already exists')
+    except Exception as e:
+        print('OpenSearchClient could not be created:', str(e))
+
+    return client
+
+
+# Create a Kafka consumer
+def create_kafka_consumer():
+    try:
+        consumer = Consumer(
+            {
+                'bootstrap.servers': 'localhost:9092',
+                'group.id': 'consumer-opensearch-demo',
+                'auto.offset.reset': 'earliest',
+                'enable.auto.commit': False
+            }
+        )
+
+        print('Kafka Consumer has been initiated...')
+    except Exception as e:
+        print('Consumer is not created:', str(e))
+    return consumer
+
+
+opensearch_client = create_opensearch_client()
+topic = 'wikimedia.recentchange'
+consumer = create_kafka_consumer()
+consumer.subscribe([topic])
+print('Consumer subscribed to the topic wikimedia.recentchange')
+
+batch_size = 10
+while True:
+    # Poll for new messages
+    messages = consumer.consume(batch_size, timeout=2)
+    messagesCount = len(messages)
+    print('Received', messagesCount, 'messages')
+
+    if not messages:
+        continue
+    for message in messages:
+        if message.error():
+            print(f"Error consuming message: {message.error()}")
+        else:
+            try:
+                message_value = message.value()
+                if message_value:
+                    decoded_value = message_value.decode('utf-8')
+                    # Split the key-value pair
+                    key, value = decoded_value.split(": ", 1)
+
+                    if key == "id":
+                        try:
+                            message_data_list = json.loads(value)
+                            for message_data in message_data_list:
+                                try:
+                                    index_request = {
+                                        'index': 'wikimedia',
+                                        'body': message_data
+                                    }
+                                    response = opensearch_client.index(**index_request)
+                                    print('Inserted one doc in OpenSearch:', response['result'], 'ID:', response['_id'])
+                                except Exception as e:
+                                    print("Error indexing document:", str(e))
+                        except json.JSONDecodeError as json_error:
+                            print("Error decoding JSON data:", str(json_error))
+                    elif key == "data":
+                        try:
+                            message_data = json.loads(value)
+                            index_request = {
+                                'index': 'wikimedia',
+                                'body': message_data
+                            }
+                            response = opensearch_client.index(**index_request)
+                            print('Inserted one doc in OpenSearch:', response['result'], 'ID:', response['_id'])
+                        except json.JSONDecodeError as json_error:
+                            print("Error decoding JSON data:", str(json_error))
+                        except Exception as e:
+                            print("Error indexing document:", str(e))
+                    else:
+                        print("Unexpected key:", key)
+                else:
+                    print("Empty message value, skipping indexing.")
+            except Exception as e:
+                print("Error decoding message:", str(e))
--- kafkaProducer.py
+++ kafkaProducer.py
@@ -1,6 +1,9 @@
+import json
+
 import requests
 from confluent_kafka import Producer
 import urllib3
+import logging
 
 # Disable SSL certificate verification (not recommended for production)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -9,10 +12,21 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 KAFKA_BROKER = "localhost:9092"
 KAFKA_TOPIC = "wikimedia.recentchange"
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
 # Create a Kafka producer
 producer_config = {
-    "bootstrap.servers": KAFKA_BROKER
-}
+    "bootstrap.servers": KAFKA_BROKER,
+    #"enable_idempotence": True,
+    "acks": "all",
+    "linger.ms": 20,
+    "queue.buffering.max.ms": 20,
+    "batch.size": str(32*1024),
+    "compression.type": 'snappy'
+     }
 producer = Producer(producer_config)
 
 # Fetch data from the Wikimedia stream without SSL verification
